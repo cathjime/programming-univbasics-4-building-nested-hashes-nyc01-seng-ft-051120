051ed1312fce2cb4c22c28184388d6e6634f9fc4
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-04-30 18:15:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
- new_hash = {:railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200 }, :names => {}}}
+ new_hash = {:railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200 }, :names => {:reading_railroa => {}, }}}
 end
 
 def monopoly_with_fourth_tier
